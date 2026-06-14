d02eb6fa75b84cfa1cd2e4823821fff8abed8bac
mao72 <kirillmao27@icloud.com>
2026-06-14 18:06:30 +0300

    feat: сделал top-bar c search и значком профиля

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,26 +4,37 @@ import { ReactNode } from 'react';
 import './mainPage.css';
 import Link from 'next/link';
 import StudentNavigationBar from '@/components/StudentNavigationBar/StudentNavigationBar';
+import Icon from '@/components/Icon/Icon';
+import SearchBar from '@/components/SearchBar/SearchBar';
 
 export default function MainLayout({ children }: { children: ReactNode }) {
     return (
         <div className="main-container">
-            <Link href="/" className="logo-cell">
+            <Link href="/" className="logo-part">
                 <img src="/logo-navy.svg" alt="sehriyo-logo"></img>
                 <span>Портал ученика</span>
             </Link>
-            <div className="search-profile-cell">
-                <div className="search-part">
-                    <input placeholder="поиск курсов"></input>
+            <div className="top-bar">
+                <SearchBar></SearchBar>
+                <div className="profile-part">
+                    <Icon
+                        size={20}
+                        name="bell"
+                        style={{
+                            alignSelf: 'center',
+                        }}
+                    ></Icon>
+                    <button className="avatar-part">
+                        <span className="avatar"> АМ </span>
+                    </button>
                 </div>
-                <div className="profile-part">профиль</div>
             </div>
             <div className="side-bar">
                 <StudentNavigationBar></StudentNavigationBar>
                 <div className="admin-panel">Смена вида</div>
             </div>
 
-            <div className="main-content-cell"> {children} </div>
+            <div className="main-content"> {children} </div>
         </div>
     );
 }
